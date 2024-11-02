dac62e2a51feaf34f82b9c308c075f35eb3c3bb8
Svenlaa <mail@svenlaa.com>
2024-11-02 22:52:01 +0100

    honooo

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = new Hono();
 
 app.get('/', (c) =>
     c.html(
-        '<style>h1>a{color:initial;text-decoration:none;&:hover{color:forestgreen}}</style><title>hono-demo by Svenlaa</title><h1><a href="https://github.com/Svenlaa/hono-demo">hono-demo</a></h1>an exploration by <a href="https://svenlaa.com">Svenlaa</a>'
+        '<style>h1>a{color:initial;text-decoration:none;&:hover{color:forestgreen}}</style><title>hono-demo by Svenlaa</title><h1><a href="https://github.com/Svenlaa/hono-demo">hono-demo</a></h1>a coding exploration by <a href="https://svenlaa.com">Svenlaa</a>'
     )
 );
 
