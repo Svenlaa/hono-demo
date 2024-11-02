d3d836f2c95ae219dec73fa4fc7a5a77bab54dc8
Svenlaa <mail@svenlaa.com>
2024-11-02 22:43:13 +0100

    pretty

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
-app.get('/', (c) => c.html('an exploration by <a href="https://svenlaa.com">Svenlaa</a>'));
+app.get('/', (c) =>
+    c.html(
+        '<style>h1>a{color:initial;text-decoration:none;&:hover{color:forestgreen}}</style><title>hono-demo by Svenlaa</title><h1><a href="https://github.com/Svenlaa/hono-demo">hono-demo</a></h1>an exploration by <a href="https://svenlaa.com">Svenlaa</a>'
+    )
+);
 
 export default app;
