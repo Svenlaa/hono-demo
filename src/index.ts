2c4bedffaa08c25083a87a4c278e2d6bebbb2f7a
Svenlaa <mail@svenlaa.com>
2024-11-02 23:10:08 +0100

    await put

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = new Hono<{ Bindings: Bindings }>();
 app.get('/', async (c) => {
     const oldCount = +((await c.env.KV_STORE.get('count')) ?? '0');
     const count = oldCount + 1;
-    c.env.KV_STORE.put('count', count.toString());
+    await c.env.KV_STORE.put('count', count.toString());
 
     return c.html(
         `<style>h1>a{color:initial;text-decoration:none;&:hover{color:forestgreen}}</style><title>hono-demo by Svenlaa</title><h1><a href="https://github.com/Svenlaa/hono-demo">hono-demo</a></h1>a coding exploration by <a href="https://svenlaa.com">Svenlaa</a><span style="padding-top:1em;display:block">you are visitor number ${count}.</span>`
